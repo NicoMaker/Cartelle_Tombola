aedfc972695105abf5aa18682bd3447512e36f0a
Nicola <93321282+NicoMaker@users.noreply.github.com>
2025-05-17 23:27:55 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -123,8 +123,8 @@ document.addEventListener("DOMContentLoaded", () => {
   async function generateCards() {
     const numSets = Number.parseInt(numSetsInput.value);
 
-    if (numSets < 1 || numSets > 27) {
-      showAlert("error", "Errore", "Inserisci un numero di set valido (1-27)");
+    if (numSets < 1 || numSets >500) {
+      showAlert("error", "Errore", "Inserisci un numero di set valido (1-500)");
       return;
     }
 
